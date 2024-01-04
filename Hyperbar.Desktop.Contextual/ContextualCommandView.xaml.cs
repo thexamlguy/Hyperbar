df602623495d679f6753e6a892141dbf9b3b3899
TheXamlGuy <dan_clark@outlook.com>
2024-01-04 22:27:54 +0000

    keyboard introp

--- Hyperbar.Desktop.Contextual/ContextualCommandView.xaml.cs
+++ Hyperbar.Desktop.Contextual/ContextualCommandView.xaml.cs
@@ -1,8 +1,15 @@
+using Hyperbar.Desktop.Win32;
 using Microsoft.UI.Xaml.Controls;
+using Windows.System;
 
 namespace Hyperbar.Desktop.Contextual;
 
 public sealed partial class ContextualCommandView : Page
 {
     public ContextualCommandView() => InitializeComponent();
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        KeyIntrop.Type((VirtualKey)186, VirtualKey.LeftWindows);
+    }
 }
--- Hyperbar.Desktop/Views/CommandView.xaml.cs
+++ Hyperbar.Desktop/Views/CommandView.xaml.cs
@@ -1,8 +1,14 @@
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace Hyperbar.Desktop;
 
 public sealed partial class CommandView : Page
 {
     public CommandView() => InitializeComponent();
+
+    protected override void OnKeyDown(KeyRoutedEventArgs e)
+    {
+        base.OnKeyDown(e);
+    }
 }
