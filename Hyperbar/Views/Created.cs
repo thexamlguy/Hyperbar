7dc125ebc714dcb6f897ad685a6fc152124f2ebe
TheXamlGuy <dan_clark@outlook.com>
2024-01-17 20:33:41 +0000

    more animation work

--- Hyperbar/Views/Created.cs
+++ Hyperbar/Views/Created.cs
@@ -7,10 +7,3 @@ public record Created<TValue>(TValue Value, object Target) : INotification
         return new Created<TValue>(value, typeof(TTarget).Name);
     }
 }
-public record Inserted<TValue>(int Index, TValue Value, object Target) : INotification
-{
-    public static Inserted<TValue> For<TTarget>(int index, TValue value)
-    {
-        return new Inserted<TValue>(index, value, typeof(TTarget).Name);
-    }
-}
---
+++ Hyperbar/Views/Inserted.cs
@@ -0,0 +1,9 @@
+﻿namespace Hyperbar;
+
+public record Inserted<TValue>(int Index, TValue Value, object Target) : INotification
+{
+    public static Inserted<TValue> For<TTarget>(int index, TValue value)
+    {
+        return new Inserted<TValue>(index, value, typeof(TTarget).Name);
+    }
+}
